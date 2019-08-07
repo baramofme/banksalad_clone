9f188ea5621b9d2749db674cdba9935b0e23bd79
Jihoon Yi <baramofme@gmail.com>
2019-08-07 18:07:45 +0900

    Input, Output VO 구조 추가

---
+++ src/models/input/ExampleIVO.ts
@@ -0,0 +1,11 @@
+export class ExampleIVO {
+  public someInput1: boolean = true;
+  public someInput2: string = "hehho";
+  public someInput3: number = 3;
+
+  constructor(someInput1: boolean, someInput2: string, someInput3: number) {
+    this.someInput1 = someInput1;
+    this.someInput2 = someInput2;
+    this.someInput3 = someInput3;
+  }
+}
---
+++ src/models/output/ExampleOVO.ts
@@ -0,0 +1,6 @@
+export class ExampleOVO {
+  public something: boolean = true;
+  constructor(res: boolean) {
+    this.something = res;
+  }
+}
